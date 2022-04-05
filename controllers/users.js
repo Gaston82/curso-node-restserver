8c0d2965162c4b3917dc6471f82e9d08a9afeab0
Gaston82 <cabezaartaza@hotmail.com>
2022-04-05 12:33:54 +0200

    add mongoose connection

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const { response, request } = require("express");
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
   const { q, name, apikey, page = 1, limit } = req.query;
@@ -20,12 +21,14 @@ const usuariosPut = (req, res) => {
   });
 };
 
-const usuariosPost = (req, res) => {
-  const { nombre, edad } = req.body;
+const usuariosPost = async (req, res) => {
+  //const { nombre, correo, password, img } = req.body;
+  const body = req.body;
+  const usuario = new Usuario(body);
+  await usuario.save();
   res.status(201).json({
     msg: "post API - controller",
-    nombre,
-    edad,
+    usuario,
   });
 };
 
