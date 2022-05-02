ea84c8538fbcc48cb3e28084624e5e5ee4d090f2
Gaston82 <cabezaartaza@hotmail.com>
2022-05-02 03:13:27 +0200

    add auth verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,51 @@
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login ok",
-  });
+const bcryptjs = require("bcryptjs");
+const { response } = require("express");
+const Usuario = require("../models/usuario");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    //verificar si el correo existe
+
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos",
+      });
+    }
+
+    // si el usuario está activo
+
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos",
+      });
+    }
+
+    //verificar contraseña
+
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos",
+      });
+    }
+
+    //generar el JWT
+
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      msg: "Login ok",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 module.exports = {
