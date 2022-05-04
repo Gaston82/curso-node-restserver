15f4c67d62f0a2660b0b30eb1a2ea97336609d33
Gaston82 <cabezaartaza@hotmail.com>
2022-05-05 01:40:11 +0200

    add jwt validation for delete option

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,43 @@
+const { response, request } = require("express");
+const jwt = require("jsonwebtoken");
+const Usuario = require("../models/usuario");
+
+const validarJWT = async (req = request, res = response, next) => {
+  const token = req.header("x-token");
+  if (!token) {
+    return res.status(401).json({
+      msg: "No hay token en la petición",
+    });
+  }
+
+  try {
+    const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+    const usuario = await Usuario.findById(uid);
+
+    //Verificamos usuario null
+    if (!usuario) {
+      return res.status(401).json({
+        msg: " Token no válido - USUARIO NO REGISTRADO EN LA bd",
+      });
+    }
+
+    //Verificar si el uid tienen estado true
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: " Token no válido - USUARIO NO REGISTRADO EN LA bd",
+      });
+    }
+
+    req.usuario = usuario;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      msg: "Token no válido",
+    });
+  }
+};
+
+module.exports = {
+  validarJWT,
+};
